a39982c4fdadbb82287e2945783f4fd8ff6cb575
Garry14 <157243411+JogaMiguelGames@users.noreply.github.com>
2025-08-23 21:30:32 -0300

    Update add.js

--- add.js
+++ add.js
@@ -1,15 +1,17 @@
+// add.js
 addCubeBtn.addEventListener('click', () => {
   const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-  const cubeMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00, flatShading: true });
-  const newCube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  const cubeMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
 
+  const newCube = new THREE.Mesh(cubeGeometry, cubeMaterial);
   newCube.position.set(0, 0, 0);
+  newCube.name = `Cube ${cubes.length}`;
   newCube.castShadow = true;
   newCube.receiveShadow = true;
-  newCube.name = `Cube-${cubes.length + 1}`;
 
   scene.add(newCube);
   cubes.push(newCube);
+
   selectedCube = newCube;
   updatePanelForCube(newCube);
   updateCubeList();
