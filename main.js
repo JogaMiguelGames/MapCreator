2264f6dcc0a879a901d034f074ba01b3753c24bd
Garry14 <157243411+JogaMiguelGames@users.noreply.github.com>
2025-08-26 10:42:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-const scene = new THREE.Scene();
-scene.background = new THREE.Color('#00BFFF');
-
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({antialias:true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-document.body.appendChild(renderer.domElement);
-
 const cubeGeometry = new THREE.BoxGeometry(1,1,1);
 const cubeMaterial = new THREE.MeshStandardMaterial({color: 0xffffff});
 const mainCube = new THREE.Mesh(cubeGeometry, cubeMaterial);
@@ -337,3 +327,4 @@ animate();
 // Inicializa UI
 updatePanelForCube(selectedCube);
 updateCubeList();
+
