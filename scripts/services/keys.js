27fc389de0c526b77a334889e209ac8896350032
Garry14 <157243411+JogaMiguelGames@users.noreply.github.com>
2025-08-28 10:35:23 -0300

    Create keys.js

---
+++ scripts/services/keys.js
@@ -0,0 +1,40 @@
+document.addEventListener("keydown", (event) => {
+  if (event.ctrlKey && event.shiftKey &&event.code === "KeyV") {
+    event.preventDefault();
+
+    const main = document.getElementById("sceneContainer");
+    const canvas = document.querySelector("canvas[data-engine='three.js r152']");
+    const bodyChildren = Array.from(document.body.children);
+
+    const isHidden = document.body.classList.contains("ui-hidden");
+
+    if (isHidden) {
+      // Restaura tudo
+      bodyChildren.forEach(el => {
+        if (el !== main && el !== canvas) {
+          el.style.display = "";
+        }
+      });
+      if (main) main.style.display = "";
+      if (canvas) canvas.style.display = "";
+      document.body.classList.remove("ui-hidden");
+    } else {
+      // Esconde tudo, menos main e canvas
+      bodyChildren.forEach(el => {
+        if (el !== main && el !== canvas) {
+          el.style.display = "none";
+        }
+      });
+      if (main) main.style.display = "";
+      if (canvas) canvas.style.display = "";
+      document.body.classList.add("ui-hidden");
+    }
+  }
+});
+
+document.addEventListener('keydown', (e) => {
+  if(e.ctrlKey && e.shiftKey && e.code === 'KeyK'){
+    linesVisible = !linesVisible;
+    axisLines.forEach(line => line.visible = linesVisible);
+  }
+});
