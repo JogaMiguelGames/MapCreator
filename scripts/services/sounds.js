0ce10ea028e275b88997948faa5e8013bb3b7ae4
Garry14 <157243411+JogaMiguelGames@users.noreply.github.com>
2025-09-07 21:30:37 -0300

    Update sounds.js

--- scripts/services/sounds.js
+++ scripts/services/sounds.js
@@ -1,1 +1,10 @@
+    const createSound = new Audio("resources/sounds/buttons/create.wav");
 
+    // Captura o botão
+    const button = document.getElementById("addCubeBtn");
+
+    // Toca o som quando o botão é clicado
+    button.addEventListener("click", () => {
+      createSound.currentTime = 0; // Reinicia o som se já estiver tocando
+      CreateSound.play();
+    });
