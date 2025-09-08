97437e3bdb354d1542cd7fa857a7d13f3784de5d
Garry14 <157243411+JogaMiguelGames@users.noreply.github.com>
2025-09-07 21:42:12 -0300

    Update sounds.js

--- scripts/services/sounds.js
+++ scripts/services/sounds.js
@@ -1,20 +1,22 @@
-const createSound = new Audio("resources/sounds/buttons/create.wav");
+document.addEventListener("DOMContentLoaded", () => {
+    const createSound = new Audio("resources/sounds/buttons/create.wav");
 
-const createCubeButton = document.getElementById("addCubeBtn");
-const createSphereButton = document.getElementById("addSphereBtn");
-const createPlaneButton = document.getElementById("addPlaneBtn");
+    const createCubeButton = document.getElementById("addCubeBtn");
+    const createSphereButton = document.getElementById("addSphereBtn");
+    const createPlaneButton = document.getElementById("addPlaneBtn");
 
-createCubeButton.addEventListener("click", () => {
-    createSound.currentTime = 0;
-    createSound.play();
-});
+    createCubeButton?.addEventListener("click", () => {
+        createSound.currentTime = 0;
+        createSound.play();
+    });
 
-createSphereButton.addEventListener("click", () => {
-    createSound.currentTime = 0;
-    createSound.play();
-});
+    createSphereButton?.addEventListener("click", () => {
+        createSound.currentTime = 0;
+        createSound.play();
+    });
 
-createPlaneButton.addEventListener("click", () => {
-    createSound.currentTime = 0;
-    createSound.play();
+    createPlaneButton?.addEventListener("click", () => {
+        createSound.currentTime = 0;
+        createSound.play();
+    });
 });
