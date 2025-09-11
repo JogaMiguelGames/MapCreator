6cb95e433fc22a65a6d8a705ad198814ca23dc25
Garry14 <157243411+JogaMiguelGames@users.noreply.github.com>
2025-09-11 20:34:31 -0300

    Update sounds.js

--- scripts/services/sounds.js
+++ scripts/services/sounds.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const createSound = new Audio("resources/sounds/buttons/create.wav");
+    const menuOpenSound = new Audio("resources/sounds/buttons/menuOpen.wav");
 
     const createCubeButton = document.getElementById("addCubeBtn");
+    const createCubeButton = document.getElementById("addCubeBtn");
     const createSphereButton = document.getElementById("addSphereBtn");
-    const createPlaneButton = document.getElementById("addPlaneBtn");
+    const fileMenuButton = document.getElementById("fileBtn");
 
     createCubeButton?.addEventListener("click", () => {
         createSound.currentTime = 0;
@@ -18,5 +20,10 @@ document.addEventListener("DOMContentLoaded", () => {
     createPlaneButton?.addEventListener("click", () => {
         createSound.currentTime = 0;
         createSound.play();
+    });
+
+    fileMenuButton?.addEventListener("click", () => {
+        menuOpenSound.currentTime = 0;
+        menuOpenSound.play();
     });
 });
