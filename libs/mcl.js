8f2bb3f654f357487570139920cc0d95d93dbaf9
Garry14 <157243411+JogaMiguelGames@users.noreply.github.com>
2025-10-29 17:19:02 -0300

    Update mcl.js

--- libs/mcl.js
+++ libs/mcl.js
@@ -36,6 +36,19 @@ const addWindowContent = document.getElementById('addWindowContent');
 const aW_CreateFolder = document.getElementById('aW_CreateFolder');
 const aW_CreateScript = document.getElementById('aW_CreateScript');
 
+// Icons
+const folder_SVG_Icon = 'resources/images/ui/icons/folder.svg';
+const kS_Script_SVG_Icon = 'resources/images/ui/icons/kerneliumScript_File.svg';
+
+const cube_SVG_Icon = 'resources/images/ui/icons/cube.svg';
+const sphere_SVG_Icon = 'resources/images/ui/icons/sphere.svg';
+
+const cube_PNG_Icon = 'resources/images/ui/icons/cube.png';
+const sphere_PNG_Icon = 'resources/images/ui/icons/sphere.png';
+const cylinder_PNG_Icon = 'resources/images/ui/icons/cylinder.png';
+const cone_PNG_Icon = 'resources/images/ui/icons/cone.png';
+const plane_PNG_Icon = 'resources/images/ui/icons/plane.png';
+
 // Selected Stuffs
 let selectedCube = mainCube;
 let selectedObject = null;
