8a8e69e9f482cc29b1eb802bf053f399f08f76e0
Garry14 <157243411+JogaMiguelGames@users.noreply.github.com>
2025-10-31 22:00:53 -0300

    Update menuStrip.js

--- scripts/services/menuStrip.js
+++ scripts/services/menuStrip.js
@@ -1,6 +1,6 @@
 // scripts/services/menuStrip.js
 import { Project, Model, Page, Tree_View, Icon } from '../../libs/mcl/mcl.js';
-import { Create } from '../../libs/mcl/add.js';
+import { CreateCube, CreateSphere, CreateCylinder, CreateCone, CreatePlane, CreateCamera, CreateLight } from '../../libs/mcl/add.js';
 
 (function(){
   const fileBtn = document.getElementById('fileMenuBtn');
