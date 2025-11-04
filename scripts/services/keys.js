02ec21af586b91e570c7e9b3b3e765db976f7654
Garry14 <157243411+JogaMiguelGames@users.noreply.github.com>
2025-11-04 12:26:48 -0300

    Update keys.js

--- scripts/services/keys.js
+++ scripts/services/keys.js
@@ -1,6 +1,6 @@
 import { Project, Model, Page, Tree_View, Icon } from '../../libs/mcl/mcl.js';
 import { CreateCube, CreateSphere, CreateCylinder, CreateCone, CreatePlane, CreateCamera, CreateLight } from '../../libs/mcl/add.js';
-import { sphereGeometrySmall, spheres, offsets, addManipulationSpheres, updateSpheresVisibility, selectedSphere, plane, offset, intersection, dragRaycaster, mouseVec, onPointerDown, updateCursor, onPointerMove, onPointerUp, linesVisible, axisLines, addAxisLine, createHugeGrid, hugeGrid, gridGroup, gridStep, gridLimit, gridColor, updateGridAroundCameraCircle} from '../../libs/mcl/objects.js';
+import { sphereGeometrySmall, spheres, offsets, addManipulationSpheres, updateSpheresVisibility, selectedSphere, plane, offset, intersection, dragRaycaster, mouseVec, onPointerDown, updateCursor, onPointerMove, onPointerUp, State, axisLines, addAxisLine, createHugeGrid, hugeGrid, gridGroup, gridStep, gridLimit, gridColor, updateGridAroundCameraCircle} from '../libs/mcl/objects.js';
 
 document.addEventListener("keydown", (event) => {
   if (event.ctrlKey && event.shiftKey &&event.code === "KeyV") {
