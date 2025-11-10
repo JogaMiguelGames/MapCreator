fd304bf5016e7410883cf5796b933b68a88fd014
Garry14 <157243411+JogaMiguelGames@users.noreply.github.com>
2025-11-10 20:06:24 -0300

    Update theme.js

--- scripts/ui/theme.js
+++ scripts/ui/theme.js
@@ -1,4 +1,17 @@
-function SetTheme(Theme) {
-  const CSSLink = document.getElementById("PageTheme");
-  CSSLink.href = Theme;
+function LoadTheme() {
+    const savedTheme = JSON.parse(localStorage.getItem('mapCreator.Theme'));
+
+    if (savedTheme) {
+        const CSSLink = document.getElementById("PageTheme");
+        CSSLink.href = savedTheme;
+    }
 }
+
+function SetTheme(themeFile) {
+    const CSSLink = document.getElementById("PageTheme");
+    CSSLink.href = themeFile;
+
+    localStorage.setItem('mapCreator.Theme', JSON.stringify(themeFile));
+}
+
+LoadTheme();
