87e8bdcfa417f366e0313ec0f5673c0b773c10c1
Garry14 <157243411+JogaMiguelGames@users.noreply.github.com>
2025-11-25 16:09:19 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -197,11 +197,15 @@ function updatePanelForCube(object3D) {
     HEX_Enabled = !HEX_Enabled;
     if (HEX_Enabled) {
       Type_Color_Button.textContent = "Set Type To RGB";
+
+      HEX_Enabled = true;
       
       Page.Elements.Input.Color.Hex_Input.disabled = true;
       Page.Elements.Input.Color.RGB.RGB_Color_Input.disabled = false;
     } else {
       Type_Color_Button.textContent = "Set Type To HEX";
+
+      HEX_Enabled = false;
       
       Page.Elements.Input.Color.Hex_Input.disabled = false;
       Page.Elements.Input.Color.RGB.RGB_Color_Input.disabled = true;
@@ -830,6 +834,7 @@ updatePanelForCube(object3D);
 UpdateTreeView();
 updateSpheresVisibility();
 loop();
+
 
 
 
