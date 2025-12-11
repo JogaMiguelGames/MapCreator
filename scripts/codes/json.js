7f4f36602da027a4c8bfa57d7b6cfeb3323addc6
Garry14 <157243411+JogaMiguelGames@users.noreply.github.com>
2025-12-11 16:48:57 -0300

    Update json.js

--- scripts/codes/json.js
+++ scripts/codes/json.js
@@ -1,16 +1,14 @@
-fetch('json/manifest.json')
+fetch('manifest.json')
   .then(response => {
     if (!response.ok) throw new Error('Failed to load manifest.json');
     return response.json();
   })
   .then(manifest => {
     if (manifest.page) {
-      // Atualiza o título da página
       if (manifest.page.title) {
         document.title = manifest.page.title;
       }
 
-      // Atualiza o ícone da página
       if (manifest.page.icon) {
         let link = document.querySelector("link[rel~='icon']");
         if (!link) {
