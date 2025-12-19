66be5884a47dbaa1511ac1d6544b6828e4f7c65f
Garry14 <157243411+JogaMiguelGames@users.noreply.github.com>
2025-12-19 18:14:01 -0300

    Update IDInput reference to use MCL namespace

--- scripts/objects/id.js
+++ scripts/objects/id.js
@@ -1,6 +1,6 @@
 import { MCL, Add } from '../../libs/mcl/Index.js';
 
-const IDInput = Page.Elements.ID.Input;
+const IDInput = MCL.Page.Elements.ID.Input;
 const ID = window.selectedObject3D.userData.id;
 
 IDInput.value = ID;
